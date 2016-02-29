307878fd87832f9355ab0611cb70b30072c99435
Nick Aversano <nick23hi@gmail.com>
2016-02-29 16:27:49 -0500

    refactored env

---
+++ test/helpers/env.js
@@ -0,0 +1,37 @@
+var rimraf = require('rimraf');
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+
+// sync helper functions for creating and destorying directories
+
+const ENV_PREFIX = __dirname + '/../tmp';
+module.exports = {
+	/**
+	 * Builds a directory for testing where env the environment name and files is a map of filename => content.
+	 * All files are created inside the directory env.
+	 * @return the path to env
+	 *     env.create('1', {'file.txt': 'hello', 'folder/file.txt': 'hi'});
+	 */
+	create: function(env, files) {
+		var prefix = path.resolve(ENV_PREFIX + '/' + env + '/');
+		for (var name in files) {
+			var file = prefix + name;
+			writeFileSync(file, files[name]);
+		}
+
+		return prefix;
+	},
+	/** Recursively removes the test folder. Use with after or afterEach */
+	destroy: function() {
+		rimraf.sync(ENV_PREFIX);
+	},
+}
+
+// helpers
+
+/** Writes the file (and any directories needed). */
+function writeFileSync(file, contents, cb) {
+	mkdirp.sync(path.dirname(file));
+	fs.writeFileSync(file, contents);
+};
--- test/index.js
+++ test/index.js
@@ -1,9 +1,6 @@
-var carbon = require('../index');
 var assert = require('assert');
-var rimraf = require('rimraf');
-var fs = require('fs');
-var path = require('path');
-var mkdirp = require('mkdirp');
+var env = require('./helpers/env');
+var carbon = require('../index');
 
 describe('TODO', function() {
 	it('add tests', function() {
@@ -11,36 +8,10 @@ describe('TODO', function() {
 	});
 
 	it('env', function() {
-		createEnv('1', {'test': 'hello', 'folder/file': 'hi'});
+		var env1 = env.create('1', {'getme': 'hello', '/file': 'hi'});
+		var env2 = env.create('2', {'test': 'hello', 'folder/file': 'hi'});
 	});
 
-	after(destroyEnv);
+	after(env.destroy);
 });
 
-// sync helper functions for creating and destorying directories
-
-const ENV_PREFIX = __dirname + '/tmp';
-/**
- * Builds a directory for testing where env the environment name and files is a map of filename => content.
- * All files are created inside the directory env.
- * @return the path to env
- */
-function createEnv(env, files) {
-	var prefix = ENV_PREFIX + '/' + env + '/';
-	for (var name in files) {
-		var file = prefix + name;
-		writeFileSync(file, files[name]);
-	}
-
-	return prefix;
-};
-
-function destroyEnv() {
-	rimraf.sync(ENV_PREFIX);
-};
-
-/** Writes the file (and any directories needed). */
-function writeFileSync(file, contents, cb) {
-	mkdirp.sync(path.dirname(file));
-	fs.writeFileSync(file, contents);
-};
